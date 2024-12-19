51551a33da190c3e97437e9434deaaea6f9576ef
Sudipta Das <shiningsudipto@gmail.com>
2024-12-19 15:24:07 +0600

    warehouse creating successfully

--- src/pages/warehouse/components/CreateWarehouse.jsx
+++ src/pages/warehouse/components/CreateWarehouse.jsx
@@ -10,6 +10,7 @@ import {
 import FormikDropdown from "../../../components/formik/FormikDropdown";
 import { toast } from "sonner";
 import { getFirstErrorMessage } from "../../../utils/error.utils";
+import CustomButton from "../../../components/ui/CustomButton";
 
 const initialValues = {
   location: "",
@@ -41,9 +42,8 @@ const CreateWarehouse = () => {
   }));
 
   const handleCreateWarehouse = async (values) => {
-    console.log(values);
+    setCreateModalOpen(false);
     const toastId = toast.loading("Creating warehouse...");
-
     // Prepare FormData
     const formData = new FormData();
     formData.append("location", values.location);
@@ -65,11 +65,10 @@ const CreateWarehouse = () => {
     try {
       const response = await CreateWarehouseFunc(formData).unwrap();
       console.log({ response });
-      //   toast.success("Warehouse created successfully!", {
-      //     id: toastId,
-      //     duration: 2000,
-      //   });
-      //   setCreateModalOpen(false);
+      toast.success("Warehouse created successfully!", {
+        id: toastId,
+        duration: 2000,
+      });
     } catch (error) {
       console.log(getFirstErrorMessage(error));
       toast.error(getFirstErrorMessage(error), {
@@ -81,7 +80,10 @@ const CreateWarehouse = () => {
 
   return (
     <div>
-      <button onClick={() => setCreateModalOpen(true)}>Create Warehouse</button>
+      <CustomButton
+        onClick={() => setCreateModalOpen(true)}
+        label="Create warehouse"
+      />
       <CustomModal open={isCreateModalOpen} setOpen={setCreateModalOpen}>
         <Formik initialValues={initialValues} onSubmit={handleCreateWarehouse}>
           {({ setFieldValue }) => {
@@ -113,12 +115,7 @@ const CreateWarehouse = () => {
                   options={warehouseOptions}
                   label="Select Warehouse Type"
                 />
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white py-2 px-4 rounded"
-                >
-                  Create
-                </button>
+                <CustomButton type="submit" label="Create" />
               </Form>
             );
           }}
