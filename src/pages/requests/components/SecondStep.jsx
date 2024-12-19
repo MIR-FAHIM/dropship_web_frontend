0b98dd05f2b80447ef8c4471f240dc487d9a4292
Sudipta Das <shiningsudipto@gmail.com>
2024-12-19 14:30:40 +0600

    header added in second step

--- src/pages/requests/components/SecondStep.jsx
+++ src/pages/requests/components/SecondStep.jsx
@@ -1,6 +1,11 @@
+import TabHeading from "./TabHeading";
 const SecondStep = () => {
   return (
     <div>
+      <TabHeading
+        title="Add Items"
+        subTitle="Add items or update items after receiving. Items added here can only be assigned"
+      />
       <p>Hello SecondStep</p>
     </div>
   );
