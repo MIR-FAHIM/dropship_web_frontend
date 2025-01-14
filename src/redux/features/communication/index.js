e5ddf96eba0ceb5bf9f567fff0d008a4f03671aa
Sudipta Das <shiningsudipto@gmail.com>
2025-01-14 13:38:32 +0600

    note creating

--- src/redux/features/communication/index.js
+++ src/redux/features/communication/index.js
@@ -2,11 +2,20 @@ import baseApi from "../../api/baseApi";
 
 const communicationApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-   
     getNotesByReq: builder.query({
       query: (id) => `/notes/${id}`,
+      providesTags: ["Note"],
+    }),
+    createNote: builder.mutation({
+      query: (noteInfo) => ({
+        url: "/notes",
+        method: "POST",
+        body: noteInfo,
+      }),
+      invalidatesTags: ["Note"],
     }),
   }),
 });
 
-export const { useGetNotesByReqQuery } = communicationApi;
+export const { useGetNotesByReqQuery, useCreateNoteMutation } =
+  communicationApi;
