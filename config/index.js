608adcc948477906df878150987bbf13dd6deb0d
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-04-15 13:33:33 +0600

    new

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 //export const imgBaseUrl = "http://127.0.0.1:8000";
-export const baseUrl = "https://apidropship.braintodo.com/public";
-export const imgBaseUrl = "https://apidropship.braintodo.com/storage/app/public";
+export const baseUrl = "https://apidropship.resellerbrain.com/public";
+export const imgBaseUrl = "https://apidropship.resellerbrain.com/storage/app/public";
